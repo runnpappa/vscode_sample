2acc28db2bc7bb84fc55e2e60aaf051f7228987c
Yuki Tanaka <yu.tanaka0321@gmail.com>
2023-12-19 14:50:34 +0900

    12/19 Arduino割り込み

--- Ltika/Ltika.ino
+++ Ltika/Ltika.ino
@@ -1,24 +1,35 @@
+// #define interruptPin 2
+boolean state = true;
+void sw_led();
+
 void setup()
 {
   // put your setup code here, to run once:
-  pinMode(3, INPUT_PULLUP);
+  // pinMode(3, INPUT_PULLUP);
+  // Serial.begin(9600);
   pinMode(9, OUTPUT);
+  pinMode(7, OUTPUT);
   digitalWrite(9, LOW);
+  digitalWrite(7, LOW);
+  attachInterrupt(digitalPinToInterrupt(2), sw_led, RISING);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  if (digitalRead(3) == HIGH)
+  while (state == true)
   {
     digitalWrite(9, HIGH);
-    delay(500);
-    digitalWrite(9, LOW);
-    delay(500);
-  }
-  else
-  {
+    delay(100);
     digitalWrite(9, LOW);
     delay(100);
   }
+  // exit(0);
+}
+
+void sw_led()
+{
+  // state != state;
+  digitalWrite(7, HIGH);
+  state = false;
 }
